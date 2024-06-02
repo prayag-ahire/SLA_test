2c3a0ff8f3b9f0ee073e692bfa34c0a4c985621f
prayag <prayagahire365@gmail.com>
2024-06-02 12:57:35 +0530

    install express, mongoose , zod library and add types.js for zod , and done some logic in index.js file

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { createUser } = require("./types");
+const app = express();
+
+app.use(express.json());
+
+app.post("/signup",function(req,res){
+// this is for creating new account
+
+})
+
+app.post("/signin",function(req,res){
+// if you have already signup then 
+
+const createPayload = req.body;
+const parsedPayload = createUser.safeParse(createPayload)
+// you want some exseption then use parse
+//you want only sucsess and failer then use safeparse
+
+if(!parsedPayload.success){
+    res.status(411).json({
+        masg:"you sent the wrong information";
+    })
+    return;
+}
+
+})
