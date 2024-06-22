996fa64d3ce412cc6f16f4c54e590da5bd5d1820
prayag <prayagahire365@gmail.com>
2024-06-22 13:46:38 +0530

    changing style

--- frontend/SLA_frontend/src/App.jsx
+++ frontend/SLA_frontend/src/App.jsx
@@ -9,11 +9,10 @@ import { BrowserRouter, Router , Route, Routes} from 'react-router-dom'
 function App() {
   // const navigate = useNavigate();
   return (
-    <div className='flex  h-svh  bg-black '>
+    <div className='flex  h-svh  bg-black items-center pl-24'>
       <BrowserRouter>
       <div ><Welcomeimg/></div>
       <div><NewUsers/></div>
-      {/* <Modal/> */}
       <Routes>
         <Route path='/login' element={<Modal/>}/>
       </Routes>
--- frontend/SLA_frontend/src/components/welcomeimg.jsx
+++ frontend/SLA_frontend/src/components/welcomeimg.jsx
@@ -1,5 +1,5 @@
 export const Welcomeimg = ()=>{
-    return <div className="flex justify-center pl-[40px] w-[764px] h-[396px] m-[32px]">
+    return <div className="flex justify-center pl-[40px] mb-40 w-[764px] h-[396px]">
         <div ><img src='../src/assets/x_home.png'></img></div>
     </div>
 } 
